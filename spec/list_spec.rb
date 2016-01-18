9e85f8aa9ed0f03a09a37ecc1b4814529c4c57ee
Riley <rstarnes9@gmail.com>
2016-01-18 15:13:11 -0800

    save for later

---
+++ spec/list_spec.rb
@@ -0,0 +1,33 @@
+require('spec_helper')
+
+describe(List) do
+  describe(".all") do
+    it("starts off with no lists") do
+      expect(List.all()).to(eq([]))
+    end
+  end
+
+  describe("#name") do
+    it("tells you its name") do
+      list = List.new({:name => "Epicodus stuff", :id => nil})
+      expect(list.name()).to(eq("Epicodus stuff"))
+    end
+  end
+
+  describe('#id') do
+    it('sets id when saved') do
+      list = List.new({name: 'Epicodus stuff', id: nil})
+      list.save()
+      expect(List.all()).to(eq([list]))
+    end
+  end
+
+  describe("#==") do
+    it("is the same list if it has the same name") do
+      list1 = List.new({:name => "Epicodus stuff", :id => nil})
+      list2 = List.new({:name => "Epicodus stuff", :id => nil})
+      expect(list1).to(eq(list2))
+    end
+  end
+
+end
